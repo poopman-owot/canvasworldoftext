a3e059a4fe3601f832a47d4aed5a2249b8b28c69
poopman- <poopmanywot@gmail.com>
2015-12-10 10:14:00 -0500

    another try

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ http = require('http'),
 socketIo = require('socket.io');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));		
-var io = socketIo.listen(app);
-app.listen(app.get('port'), function() {
+var server =  http.createServer(app);
+var io = socketIo.listen(server);
+server.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
