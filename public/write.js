9d1a549ad080c7d8fefbf85fee63a77449f71c8e
poopman- <poopmanywot@gmail.com>
2016-01-17 14:02:40 -0500

    prevents blinks but not perfect.

--- public/write.js
+++ public/write.js
@@ -132,7 +132,15 @@ setInterval(function(){
 	if(stage.children.length !== 2){
 stage.removeChild(stage.children[stage.children.length-1])
 }
+for(i = dragContainer.children.length; i>0;i--){
+if(typeof dragContainer.children[i] !== "undefined"){
+if(typeof dragContainer.children[i].graphics !== "undefined" && typeof dragContainer.children[i].graphics.a()._instructions[2] !== "undefined" && dragContainer.children[i].graphics.a()._instructions[2].params[1] == "#ffffff"){
 
+dragContainer.children.splice([i], 1);
+
+}
+}
+}
 
 	clearContainer = false;
 }},100)
@@ -148,17 +156,6 @@ stage.removeChild(stage.children[stage.children.length-1])
 		
 		socket.on('clearContiner', function(){
 clearContainer=true;
-setTimeout(function(){
-for(i = dragContainer.children.length; i>0;i--){
-if(typeof dragContainer.children[i] !== "undefined"){
-if(typeof dragContainer.children[i].graphics !== "undefined" && typeof dragContainer.children[i].graphics.a()._instructions[2] !== "undefined" && dragContainer.children[i].graphics.a()._instructions[2].params[1] == "#ffffff"){
-
-dragContainer.children.splice([i], 1);
-
-}
-}
-}
-},5000)
 		})
 	
 //		Create two canvases the size of the window.
